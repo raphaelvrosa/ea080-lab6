f4f81c7a68e7c6b6fa82fcea1bbea3f81db98692
Raphael Vicente Rosa <raphaelvrosa@gmail.com>
2018-10-18 11:26:17 -0300

    fixes go.sh script

--- go.sh
+++ go.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "Killing any existing rogue AS"
-./stop_rogue.sh
+echo "Killing any existing AS"
+./back.sh
 
-echo "Starting rogue AS"
+echo "Starting AS"
 `/usr/lib/quagga/zebra -f conf/zebra-R4.conf -d -i /tmp/zebra-R4.pid > logs/R4-zebra-stdout`
 `/usr/lib/quagga/bgpd -f conf/bgpd-R4.conf -d -i /tmp/bgpd-R4.pid > logs/R4-bgpd-stdout`
